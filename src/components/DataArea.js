b3b00a5e66606f1dc10147c83d44733f9bb985c0
Nayeli Cervantes <nayelicervantes40@gmail.com>
2021-06-12 19:07:48 -0500

    file restructure

--- src/components/DataArea.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const DataArea = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
--- src/components/SearchName.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const SearchName = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
