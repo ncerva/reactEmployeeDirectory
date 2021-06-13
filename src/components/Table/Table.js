8c833f8b72dc200f8a40f57064fe74e52af2655f
Nayeli Cervantes <nayelicervantes40@gmail.com>
2021-06-12 23:04:35 -0500

    update

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -10,61 +10,6 @@ class Table extends React.Component {
         search: ""
     }
 
-    componentDidMount() {
-        API.ApiSearch()
-        .then(response => {
-            this.setState({
-                results: response.data.results
-            });
-        });
-    }
-
-    handleInputChange = e => {
-        if (e.target.name === "search") {
-            const searchTerm = e.target.value.toLowerCase();
-            this.setState({
-                search: searchTerm
-            })
-        } 
-    }
-// sort first name alphabetically
-    sortByFirstName = () => {
-        const sortedEmployees = this.state.results.sort((a, b) => {
-            if (b.name.first > a.name.first) {
-                return -1
-            }
-            if (a.name.first > b.name.first) {
-                return 1
-            }
-            return 0;
-        });
-        if (this.state.sortOrder === "descending") {
-            sortedEmployees.reverse();
-            this.setState({ sortOrder: "ascending "});
-        } else {
-            this.setState({ sortOrder: "descending "});
-        }
-        this.setState({ results: sortedEmployees })
-    }
-// sort last name alphabetically
-    sortByLasttName = () => {
-        const sortedEmployees = this.state.results.sort((a, b) => {
-            if (b.name.last > a.name.last) {
-                return -1
-            }
-            if (a.name.last > b.name.last) {
-                return 1
-            }
-            return 0;
-        });
-        if (this.state.sortOrder === "descending") {
-            sortedEmployees.reverse();
-            this.setState({ sortOrder: "ascending "});
-        } else {
-            this.setState({ sortOrder: "descending "});
-        }
-        this.setState({ results: sortedEmployees })
-    }
     render () {
         return (
             <div>
@@ -74,10 +19,8 @@ class Table extends React.Component {
                     <table className="table table-striped table-resposive text-center table-hover">
                         <thead>
                             <tr>
-                                <th>Image</th>
-                                <th>Fist Name <span className="downArrow" onClick={this.sortByFirstName}></span></th>
+                                <th>First Name <span className="downArrow" onClick={this.sortByFirstName}></span></th>
                                 <th>Last Name <span className="downArrow" onClick={this.sortByLasttName}></span></th>
-                                <th>Gender</th>
                                 <th>Phone Number</th>
                                 <th>Email</th>
                                 <th>Date of Birth</th>
